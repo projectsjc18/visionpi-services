ad41df46ef83dd5f65ce1b3328ec23bc8f335c51
projectsjc18 <44483299+projectsjc18@users.noreply.github.com>
2020-02-16 01:45:18 -0600

    Update database name

--- monitoring-service/src/config/config.js
+++ monitoring-service/src/config/config.js
@@ -2,7 +2,7 @@ console.log("Loading settings monitoring");
 const dbSettings = {
   user: process.env.MONGO_USERNAME || 'visionpi',
   password: process.env.MONGO_PASSWORD || 'visionpiDev2019',
-  name: process.env.MONGO_DB || 'visionpi_monitoring',
+  name: process.env.MONGO_DB || 'visionpiMonitoringDB',
   host: process.env.MONGO_HOSTNAME || 'localhost',
   port_db: process.env.MONGO_PORT || '27017',
   useNewUrlParser: true,
