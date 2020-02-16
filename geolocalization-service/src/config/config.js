65ea4221319e4310afd047c1b4ec3140f472ae71
projectsjc18 <44483299+projectsjc18@users.noreply.github.com>
2020-02-16 01:29:09 -0600

    Update database name

--- geolocalization-service/src/config/config.js
+++ geolocalization-service/src/config/config.js
@@ -2,11 +2,12 @@ console.log("Loading settings monitoring");
 const dbSettings = {
   user: process.env.MONGO_USERNAME || 'visionpi',
   password: process.env.MONGO_PASSWORD || 'visionpiDev2019',
-  name: process.env.MONGO_DB || 'visionpi_geolocalization',
+  name: process.env.MONGO_DB || 'visionpiGeolocalizationDB',
   host: process.env.MONGO_HOSTNAME || 'localhost',
   port_db: process.env.MONGO_PORT || '27017',
   useNewUrlParser: true,
-  collection: 'tours',
+  accountsCollection: 'accounts',
+  checkpointsCollection: 'checkpoints',
   authentication: process.env.AUTHENTICATION_SERVICE || "localhost"
 }
 
