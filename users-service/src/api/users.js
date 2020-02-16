2d482a3b9c86e3308a20f23b7c361d4ec861c86d
projectsjc18 <44483299+projectsjc18@users.noreply.github.com>
2020-02-16 01:49:12 -0600

    Update services

--- users-service/src/api/users.js
+++ users-service/src/api/users.js
@@ -1,22 +1,14 @@
 'use strict'
-const axios = require('axios')
 const status = require('http-status')
-const authenticationService = "http://authentication-service:9876";
 
 module.exports = (app, options) => {
   const {repo} = options
 
-  app.get('/v1/users', isAuthenticated, (req, res, next) => {
+  app.get('/v1/users', (req, res, next) => {
     repo.getAllUsers().then(users => {
       res.status(status.OK).json(users)
     }).catch(next)
   })
-
-  // app.get('/users/privileges', (req, res, next) => {
-  //   repo.getUsersPrivileges().then(users => {
-  //     res.status(status.OK).json(users)
-  //   }).catch(next)
-  // })
 
   app.get('/v1/users/:id', (req, res, next) => {
     repo.getUserById(req.params.id).then(user => {
@@ -30,38 +22,17 @@ module.exports = (app, options) => {
     }).catch(next)
   })
 
-  app.post('/v1/users/user', isAuthenticated, (req, res, next) => {
+  app.get('/v1/users/account/:account', (req, res, next) => {
+    repo.getUserByUserAccount(req.params.account).then(useraccount => {
+      console.log("useraccount: ------");
+      console.log(useraccount);
+      res.status(status.OK).json(useraccount)
+    }).catch(next)
+  })
+
+  app.post('/v1/users', (req, res, next) => {
     repo.saveUser(req.body).then(user => {
       res.status(status.OK).json(user)
     }).catch(next)
   })
-
-  function isAuthenticated(req, res, next) {
-    //console.log("Req");
-    //console.log(req);
-    if(req.header('Whisky') != undefined && req.header('Whisky') == "remy"){
-      next();
-    }else{
-      const config = {
-        headers: {
-          'Content-Type': 'application/json', //req.header('Content-Type'),
-          'Authorization': req.header('Authorization')
-        }
-      };
-      console.log("Config");
-      console.log(config);
-      axios.get(`${authenticationService}/auth/authenticate`, config)
-      .then(response => {
-        console.log(response.data.token);
-        next()
-      })
-      .catch(err => {
-        console.log("Error isAuthenticate");
-        console.log(err);
-        res.status(401).send({ error: { code: 401, message: 'Something failed!' }});
-        //res.send('No Authorized!');
-     });
-   }
-  }
-
 }
