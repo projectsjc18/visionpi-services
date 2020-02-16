6374080ee6fb274941b6eba1baf78c3b5a3f5f67
projectsjc18 <44483299+projectsjc18@users.noreply.github.com>
2020-02-16 01:30:10 -0600

    Add account services

--- geolocalization-service/src/api/geolocalization.js
+++ geolocalization-service/src/api/geolocalization.js
@@ -12,9 +12,30 @@ module.exports = (app, options) => {
   })
 
   /* Get checkpoint by Id */
-  app.get('/v1/geolocalization/checkpoints/:id', (req, res, next) => {
-    repo.getCheckpointById(req.params.id).then(checkpoint => {
+  app.get('/v1/geolocalization/checkpoints/:route', (req, res, next) => {
+    repo.getCheckpointsByRoute(req.params.route).then(checkpoint => {
       res.status(status.OK).json(checkpoint)
+    }).catch(next)
+  })
+
+  /* Get all accounts */
+  app.get('/v1/geolocalization/accounts', (req, res, next) => {
+    repo.getAccounts().then(accounts => {
+      res.status(status.OK).json(accounts)
+    }).catch(next)
+  })
+
+  /* Get all accounts */
+  app.get('/v1/geolocalization/accounts/details', (req, res, next) => {
+    repo.getAccountsDetails().then(accounts => {
+      res.status(status.OK).json(accounts)
+    }).catch(next)
+  })
+
+  /* Save new account */
+  app.post('/v1/geolocalization/accounts', (req, res, next) => {
+    repo.saveAccount(req.body).then(account => {
+      res.status(status.OK).json(account)
     }).catch(next)
   })
 
